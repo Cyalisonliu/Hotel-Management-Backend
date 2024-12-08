b207b64063caa4b9e85c924e5e50e3bd85597f59
Alison <75669495+Cyalisonliu@users.noreply.github.com>
2024-12-08 14:53:09 +0800

    refactor: remove AppService, update AppController, and configure TypeORM with MySQL; add Swagger documentation setup for backend test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hotel } from './hotels/hotels.entity';
+import { HotelsModule } from './hotels/hotels.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USER || 'appuser',
+      password: process.env.DB_PASSWORD || 'appuserpassword',
+      database: process.env.DB_NAME || 'line_travel',
+      entities: [Hotel],
+      synchronize: true,
+    }),
+    HotelsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
